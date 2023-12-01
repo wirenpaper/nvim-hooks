9017183d3b09a845df936e7c438a2dfd5a380886
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2023-12-01 22:03:25 +0500

    minor bugs fixed

--- hooks.lua
+++ hooks.lua
@@ -32,10 +32,11 @@ local function rehook()
 	if not file_exists(hooks) then 
 		print("hooks doesn't exist") 
 	else
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(-1)]])
+		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(-1,-1)]])
 	end
 end
 
+ERROR_LINE = 0
 kill_flag = false
 file_line_number = {}
 function lines_from(file, n)
@@ -46,6 +47,7 @@ function lines_from(file, n)
 	for line in io.lines(file) do
 		if dups[line] ~= nil and dups[line] ~= ""  then
 			print("DUPLICATE hooks:"..#lines+1)
+			ERROR_LINE = #lines+1
 			kill_flag = true
 			return
 		else
@@ -74,43 +76,59 @@ local function format_path(str)
 	end
 end
 
-local function signs(n)
-	if n == 0 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(-1)]])
-	elseif n == 1 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(0)]])
-	elseif n == 2 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(1)]])
-	elseif n == 3 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(2)]])
-	elseif n == 4 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(3)]])
-	elseif n == 5 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(4)]])
-	elseif n == 6 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(5)]])
-	elseif n == 7 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(6)]])
-	elseif n == 8 then
-		vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(7)]])
-	end
+-- #TODO vimscript -> lua
+vim.cmd([[
+function! PlaceSigns(n,m)
+	let signs = ['j', 'k', 'l', ';', 'm', ',', '.', '/', '!!', '!!', '!!', '!!', '!!']
+	let current_buffer = bufnr('%')
+
+	if a:n != -1 && a:n == a:m
+		let signs[a:n] = 'X*'
+	else
+		if a:m != -1
+			let signs[a:m] = 'XX'
+		endif
+
+		if a:n != -1
+			let signs[a:n] = signs[a:n].'*'
+		endif
+	endif
+
+	let i = 1
+	for sign in signs
+		execute 'sign define sign' . i . ' text=' . sign . ' texthl=Search'
+		execute 'sign place ' . i . ' line=' . i . ' name=sign' . i . ' buffer=' . current_buffer
+		let i += 1
+	endfor
+endfunction
+]])
+
+local function signs(n,m)
+	vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call 
+	\PlaceSigns(]] .. n-1 .. [[, ]] .. m-1 .. [[)]])
 end
 
 lines_from(hooks,1)
+n_shad = file_line_number[vim.api.nvim_buf_get_name(0)]
 local function hook_file()
-	--if file_exists(current_buffer) == true then
-	vim.cmd("on")
+	vim.cmd("silent on")
 	local path, args = format_path(current_buffer)
 	if vim.fn.isdirectory(path) == 0 then 
 		local n = file_line_number[vim.api.nvim_buf_get_name(0)]
 		if n ~= nil then
-			signs(n)
+			if ERROR_LINE ~= 0 then
+				signs(n_shad, ERROR_LINE)
+			else
+				signs(n, ERROR_LINE)
+				n_shad = n
+			end
 		else
-			signs(0)
+			signs(n_shad, ERROR_LINE)
 		end
 	end
 	vim.cmd("e "..hooks)
 	bufname[vim.api.nvim_get_current_buf()] = hooks
+	ERROR_LINE = 0
 end
 
 local function hook_term()
@@ -150,8 +168,10 @@ local function hook_mode2(n, args)
 		end
 	elseif file_exists(path) then
 		print("FILES CANNOT BE LABELED hooks:"..n)
+		ERROR_LINE = n
 	else
 		print("MALFORMED hooks:"..n)
+		ERROR_LINE = n
 	end
 end
 
@@ -169,6 +189,12 @@ local function hook_mode1(n)
 			end
 		end
 	elseif file_exists(path) then
+		if path == hooks then
+			print("cannot reference myself")
+			ERROR_LINE = n
+			return
+		end
+
 		if buffers[path] == nil then
 			vim.cmd("e "..path)
 			buffers[path] = vim.api.nvim_get_current_buf()
@@ -178,28 +204,11 @@ local function hook_mode1(n)
 		end
 	else
 		print("MALFORMED hooks:"..n)
+		ERROR_LINE = n
 	end
 end
 
-vim.cmd([[
-	function! PlaceSigns(n)
-		let signs = ['j', 'k', 'l', ';', 'm', ',', '.', '/', '!!', '!!', '!!', '!!', '!!']
-		let current_buffer = bufnr('%')
-		if a:n != -1
-			let signs[a:n] = signs[a:n].'*'
-		endif
-      
-		let i = 1
-		for sign in signs
-			execute 'sign define sign' . i . ' text=' . sign . ' texthl=Search'
-			execute 'sign place ' . i . ' line=' . i . ' name=sign' . i . ' buffer=' . 
-			\current_buffer
-			let i += 1
-		endfor
-	endfunction
-]])
-
-vim.cmd([[autocmd CursorMoved,BufWritePost,BufWinEnter hooks call PlaceSigns(-1)]])
+vim.cmd([[autocmd InsertEnter hooks call PlaceSigns(-1,-1)]])
 
 local function hook(n)
 	vim.cmd("silent on")
@@ -222,7 +231,12 @@ local function hook(n)
 		return
 	end
 	if args == nil then hook_mode1(n) else hook_mode2(n, args) end
-	signs(n)
+	if ERROR_LINE ~= 0 then
+		signs(n_shad, ERROR_LINE)
+	else
+		signs(n,ERROR_LINE)
+		n_shad = n
+	end
 end
 
 local function copy_filename()
@@ -260,3 +274,4 @@ vim.keymap.set('n', 'fn', function() pfname() end, {})
 
 -- commands
 vim.api.nvim_create_user_command("ReHook", function() rehook() end, {})
+vim.api.nvim_create_user_command("Lsf", function() vim.cmd([[filter !/\s/ ls]]) end, {})
